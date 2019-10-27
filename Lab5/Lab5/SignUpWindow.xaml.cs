a3c607b884aaabff1a159d49dc3a0cfb764ccd4e
Danial RahimZadeh <danial97rhz@outlook.com>
2019-10-27 12:08:37 +0100

    Sign up window added

---
+++ Lab5/Lab5/SignUpWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Lab5
+{
+    /// <summary>
+    /// Interaction logic for SignUpWindow.xaml
+    /// </summary>
+    public partial class SignUpWindow : Window
+    {
+        string Email;
+        public SignUpWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SignupButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!VaildEmail.IsValidEmail(Email))
+            {
+                MessageBox.Show("Skriv in giltig e-postadress", "Ogiltig email");
+            }
+        }
+
+        private void EmailTxt_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Email = this.EmailTxt.Text;
+        }
+    }
+}

9757ada439fe03e506b24d244ea0e4f962b9c81a
Jonathan Tran <jonathan.t79@hotmail.com>
2019-11-01 12:00:20 +0100

    konfirmation 2.0

--- Lab5/Lab5/SökStugor.xaml.cs
+++ Lab5/Lab5/SökStugor.xaml.cs
@@ -20,8 +20,8 @@ namespace Lab5
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
-
+            konfirmation konfirm = new konfirmation();
+            konfirm.ShowDialog();
 
         }
 
